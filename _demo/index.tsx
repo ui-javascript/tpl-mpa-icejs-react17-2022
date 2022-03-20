84cda144053c55f1f9b64055cff9e87b394b12dc
luo0412 <1095847440@qq.com>
2022-03-20 12:22:11 +0800

    first commit

---
+++ _demo/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function Dashboard() {
+  return (
+    <div>
+      <h2>Demo Page</h2>
+    </div>
+  );
+}
+
+ReactDOM.render(<Dashboard />, document.getElementById("ice-container"))
